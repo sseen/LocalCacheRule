12122632ee01a289dc000eb5a5d4c7914a0069a7
sseen <ssnfee@gmail.com>
2016-06-25 22:43:50 +0800

    空的

--- LocalCacheRule/ViewController.swift
+++ LocalCacheRule/ViewController.swift
@@ -8,11 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var mainTable: UITableView!
+    
+    var dataList:NSMutableArray! = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let refreshController = UIRefreshControl()
+        refreshController.addTarget(self, action: #selector(refreshTable), forControlEvents: .ValueChanged)
+        mainTable.addSubview(refreshController)
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +28,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func refreshTable() {
+        
+    }
 
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dataList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell0")
+        
+        if dataList.count > 0 {
+            
+        }
+        
+        return cell!
+    }
 }
 
